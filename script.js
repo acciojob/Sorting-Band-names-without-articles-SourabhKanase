703d5e339fc1490c5a2caf76afbdd0bf5478dff9
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-09-14 17:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
-
+let arr = ['the Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let regex=/\bthe\b|\ba\b\ban\b/gi;
+let ans=[];
+let obj={};
+for(let i=0;i<arr.length;i++)
+{
+	let without=arr[i].replace(regex,"").trim();
+	ans.push(without);
+    obj[without]=arr[i];
+}
+// console.log(obj);
+ans.sort();
+let final=[];
+for(let i=0;i<arr.length;i++)
+{
+  let x=obj[ans[i]];
+  final[i]=x;
+}
+console.log(final);
